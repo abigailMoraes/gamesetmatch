a8b0760faed71dd9b13083bf4543596e7eacf6c9
l8k1b <abigail.theresa.moraes@gmail.com>
2022-03-16 00:25:15 -0700

    resolved merge conflicts

--- src/main/java/com/zoomers/GameSetMatch/controller/UserController.java
+++ src/main/java/com/zoomers/GameSetMatch/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.zoomers.GameSetMatch.controller;
 
-import com.zoomers.GameSetMatch.entity.Employee;
+import com.zoomers.GameSetMatch.entity.User;
 import com.zoomers.GameSetMatch.repository.UserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -24,18 +26,21 @@ public class UserController {
     @PostMapping("/employee")
     User newEmployee(@RequestBody User newEmployee) {
         return repository.save(newEmployee);
-    @GetMapping("/all")
-    public List<User> getAll(){
-        return repository.findAll();
     }
 
-    @GetMapping("/match/{id}")
-    public List<User> getMatchParticipants(@PathVariable int id){
-        return repository.findMatchParticipantInfo(id);
-    }
+        @GetMapping("/all")
+        public List<User> getAll () {
+            return repository.findAll();
+        }
 
-    @PostMapping("/new")
-    User newUser(@RequestBody User newUser) {
-        return repository.save(newUser);
-    }
+        @GetMapping("/match/{id}")
+        public List<User> getMatchParticipants ( @PathVariable int id){
+            return repository.findMatchParticipantInfo(id);
+        }
+
+        @PostMapping("/new")
+        User newUser (@RequestBody User newUser){
+            return repository.save(newUser);
+        }
+
 }
