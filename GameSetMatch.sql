985c1009e6cc6da981d794fe8f85ad3595fb9f4c
gabouryn <ngaboury@gmail.com>
2022-03-16 17:20:37 -0700

    fix: place firebase token in resources folder, fix cases in sql for use in case sensitive sytems, add data to some columns

--- GameSetMatch.sql
+++ GameSetMatch.sql
@@ -2,13 +2,13 @@
 CREATE TABLE User(userID int NOT NULL AUTO_INCREMENT, firebase_id varchar(100), name varchar(80), email varchar(319), is_admin int, PRIMARY KEY(userID));
 
 /*Populate user_info table with sample data*/
-INSERT INTO user(firebase_id, name, email, is_admin) values ('hcarloni','Heike Carloni', 'hcarloni@gmail.com', 1);
-INSERT INTO user(firebase_id, name, email, is_admin) values ('mbaron', 'Michael Baron','mbaron@gmail.com', 0);
-INSERT INTO user(firebase_id, name, email, is_admin) values ('msanders', 'Michelle Sanders', 'msanders@gmail.com', 1);
-INSERT INTO user(firebase_id, name, email, is_admin) values ('efiaro', 'Ester Fiaro', 'efiaro@gmail.com', 0);
-INSERT INTO user(firebase_id, name, email, is_admin) values ('pghosh', 'Prabhat Ghosh', 'pghosh@gmail.com',0);
-INSERT INTO user(firebase_id, name, email, is_admin) values ('amorales', 'Alberto Morales','amorales@gmail.com', 0);
-INSERT INTO user(firebase_id, name, email, is_admin) values ('echu', 'Eileen Chu', 'echu@gmail.com', 0);
+INSERT INTO User(firebase_id, name, email, is_admin) values ('hcarloni','Heike Carloni', 'hcarloni@gmail.com', 1);
+INSERT INTO User(firebase_id, name, email, is_admin) values ('mbaron', 'Michael Baron','mbaron@gmail.com', 0);
+INSERT INTO User(firebase_id, name, email, is_admin) values ('msanders', 'Michelle Sanders', 'msanders@gmail.com', 1);
+INSERT INTO User(firebase_id, name, email, is_admin) values ('efiaro', 'Ester Fiaro', 'efiaro@gmail.com', 0);
+INSERT INTO User(firebase_id, name, email, is_admin) values ('pghosh', 'Prabhat Ghosh', 'pghosh@gmail.com',0);
+INSERT INTO User(firebase_id, name, email, is_admin) values ('amorales', 'Alberto Morales','amorales@gmail.com', 0);
+INSERT INTO User(firebase_id, name, email, is_admin) values ('echu', 'Eileen Chu', 'echu@gmail.com', 0);
 
 
 /*Create table statement for Tournament*/
@@ -19,13 +19,15 @@ ALTER TABLE Tournament ADD match_duration int;
 ALTER TABLE Tournament ADD number_of_matches int;
 ALTER TABLE Tournament ADD type varchar(60);
 ALTER TABLE Tournament ADD admin_hosts_tournament int;
+ALTER TABLE Tournament ADD FOREIGN KEY (admin_hosts_tournament) REFERENCES User(userID)
 
-INSERT INTO Tournament(name, format, location, description, max_participants, min_participants, prize, start_date, close_registration_date, round_duration) values ('Mariokart Madness', 'single knockout elimination','West Atrium room 203', 'Come join us for some krazy karting! (Individual)', 32, 4,'250$ Steam Gift Card', '2022-02-20', '2022-04-01', 7200);
-INSERT INTO Tournament(name, format, location, description, max_participants, min_participants, prize, start_date, close_registration_date, round_duration) values ('Amongus Royale', 'single knockout elimination', 'Auditorium 6, North Wing', 'Come join us for some space murder! (individual)', 10, 6, '50$ Steam Gift Card', '2022-03-04', '2022-04-15', 3600);
-INSERT INTO Tournament(name, format, location, description, max_participants, min_participants, prize, start_date, close_registration_date, round_duration) values('Spike or Bust Tournament', 'double knockout elimination', 'Volleyball court 3', 'Bump. Set. Spike. All it takes is all you have got! (2 player teams)', 16, 2, '50$ GoSport Canada Gift Card', '2022-03-09', '2022-04-21', 14400);
-INSERT INTO Tournament(name, format, location, description, max_participants, min_participants, prize, start_date, close_registration_date, round_duration) values ('StreetFighter V Tournament', 'Round Robin', 'Meeting Room 143, Basement', 'Choose your hero and test your skills!(individual)', 8, 4,'50$ GoSport Canada Gift Card', '2022-04-03', '2022-04-27', 4320);
-INSERT INTO Tournament(name, format, location, description, max_participants, min_participants, prize, start_date, close_registration_date, round_duration) values('Amazon Chess Championship', 'single knockout elimination', 'Meeting Room 13, Basement', 'Come battle your wits against the best and the brightest!', 32, 4, '100$ Amazon Gift Card', '2022-04-05', '2022-04-13', 7200);
+INSERT INTO Tournament(name, format, location, description, max_participants, min_participants, prize, start_date, close_registration_date, round_duration, admin_hosts_tournament) values ('Mariokart Madness', 'single knockout elimination','West Atrium room 203', 'Come join us for some krazy karting! (Individual)', 32, 4,'250$ Steam Gift Card', '2022-02-20', '2022-04-01', 7200, 1);
+INSERT INTO Tournament(name, format, location, description, max_participants, min_participants, prize, start_date, close_registration_date, round_duration, admin_hosts_tournament) values ('Amongus Royale', 'single knockout elimination', 'Auditorium 6, North Wing', 'Come join us for some space murder! (individual)', 10, 6, '50$ Steam Gift Card', '2022-03-04', '2022-04-15', 3600, 1);
+INSERT INTO Tournament(name, format, location, description, max_participants, min_participants, prize, start_date, close_registration_date, round_duration, admin_hosts_tournament) values('Spike or Bust Tournament', 'double knockout elimination', 'Volleyball court 3', 'Bump. Set. Spike. All it takes is all you have got! (2 player teams)', 16, 2, '50$ GoSport Canada Gift Card', '2022-03-09', '2022-04-21', 14400, 1);
+INSERT INTO Tournament(name, format, location, description, max_participants, min_participants, prize, start_date, close_registration_date, round_duration, admin_hosts_tournament) values ('StreetFighter V Tournament', 'Round Robin', 'Meeting Room 143, Basement', 'Choose your hero and test your skills!(individual)', 8, 4,'50$ GoSport Canada Gift Card', '2022-04-03', '2022-04-27', 4320, 1);
+INSERT INTO Tournament(name, format, location, description, max_participants, min_participants, prize, start_date, close_registration_date, round_duration, admin_hosts_tournament) values('Amazon Chess Championship', 'single knockout elimination', 'Meeting Room 13, Basement', 'Come battle your wits against the best and the brightest!', 32, 4, '100$ Amazon Gift Card', '2022-04-05', '2022-04-13', 7200, 1);
 
+Update Tournament SET number_of_matches=1;
 /*Create table statement for Availability*/
 CREATE TABLE Availability(userID int, tournamentID int, date DATE, availability_binary int, PRIMARY KEY(userID, tournamentID, date), FOREIGN KEY (userID) REFERENCES User(userID), FOREIGN KEY (tournamentID) REFERENCES Tournament(tournamentID));
 
@@ -40,13 +42,13 @@ INSERT INTO Round_Has(duration, type, tournamentID) values (7,'Final',1);
 CREATE TABLE Match_Has(matchID int NOT NULL AUTO_INCREMENT, result int, start_time DATETIME, end_time DATETIME, duration int, roundID int, PRIMARY KEY(matchID), FOREIGN KEY(roundID) REFERENCES Round_Has(roundID));
 
 /*Create table statement for Admin_hosts_tournament*/
-CREATE TABLE Admin_hosts_tournament(userID int, tournamentID int, PRIMARY KEY(userID, tournamentID), FOREIGN KEY (userID) REFERENCES user(userID), FOREIGN KEY (tournamentID) REFERENCES Tournament(tournamentID));
+CREATE TABLE Admin_hosts_tournament(userID int, tournamentID int, PRIMARY KEY(userID, tournamentID), FOREIGN KEY (userID) REFERENCES User(userID), FOREIGN KEY (tournamentID) REFERENCES Tournament(tournamentID));
 
 /*Create table statement for user_involves_match*/
 CREATE TABLE User_involves_match(userID int, matchID int, PRIMARY KEY(userID, matchID), FOREIGN KEY (userID) REFERENCES User(userID), FOREIGN KEY (matchID) REFERENCES Match_Has(matchID));
 
 /*Create table User_registers_tournament*/
-CREATE TABLE User_registers_tournament(userID int, tournamentID int, skill_level int, PRIMARY KEY (userID, tournamentID), FOREIGN KEY (userID) REFERENCES User(userID), FOREIGN KEY (tournamentID) REFERENCES tournament(tournamentID));
+CREATE TABLE User_registers_tournament(userID int, tournamentID int, skill_level int, PRIMARY KEY (userID, tournamentID), FOREIGN KEY (userID) REFERENCES User(userID), FOREIGN KEY (tournamentID) REFERENCES Tournament(tournamentID));
 
 /*Create table statement for Invitation Code*/
 CREATE TABLE Invitation_Code ( invitationCode varchar(10) NOT NULL, isValid tinyint(1) NOT NULL, createdOn varchar(30) NOT NULL, UNIQUE KEY invitationCode (invitationCode));
