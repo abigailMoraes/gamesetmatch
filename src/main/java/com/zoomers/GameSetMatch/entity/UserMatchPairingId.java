0879c1a054fd679482ad83dde3aed02f8de8337c
abigailMoraes <98375256+abigailMoraes@users.noreply.github.com>
2022-03-17 22:06:03 -0700

    Update UserMatchPairingId.java

--- src/main/java/com/zoomers/GameSetMatch/entity/UserMatchPairingId.java
+++ src/main/java/com/zoomers/GameSetMatch/entity/UserMatchPairingId.java
@@ -14,7 +14,7 @@ public class UserMatchPairingId implements Serializable {
     private Integer userID;
     private Integer matchID;
 
-    public UserMatchPairingId(Long userID, Long matchID){
+    public UserMatchPairingId(Integer userID, Integer matchID){
         this.userID = userID;
         this.matchID = matchID;
     }
