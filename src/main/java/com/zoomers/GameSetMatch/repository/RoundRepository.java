b4efbbc26666ddabfa9acfe33431cdd2fa10298e
l8k1b <abigail.theresa.moraes@gmail.com>
2022-03-21 18:10:04 -0700

    this would return all the rounds that for a specific tournament GET /tournaments/{tournamentID}/rounds

--- src/main/java/com/zoomers/GameSetMatch/repository/RoundRepository.java
+++ src/main/java/com/zoomers/GameSetMatch/repository/RoundRepository.java
@@ -2,9 +2,15 @@ package com.zoomers.GameSetMatch.repository;
 
 import com.zoomers.GameSetMatch.entity.Round;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface RoundRepository extends JpaRepository<Round, Integer>{
+
+    @Query(value="SELECT * FROM round_has WHERE round_has.tournamentID = :tournamentID",nativeQuery = true)
+    List<Round> getRoundsByID(int tournamentID);
 
 }
