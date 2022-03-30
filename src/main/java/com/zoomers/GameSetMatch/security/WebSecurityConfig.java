00de30bb523dca80996c8491d1c8dd5a643d7bee
Jk Local <jacky.zheng27@gmail.com>
2022-03-30 16:16:29 -0700

    feat: basic security, needs more work

---
+++ src/main/java/com/zoomers/GameSetMatch/security/WebSecurityConfig.java
@@ -0,0 +1,41 @@
+package com.zoomers.GameSetMatch.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestFilter;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String[] AUTH_WHITELIST = {"/api/verifyIdToken"};
+
+//    @Override
+//    public void configure(WebSecurity web) {
+//        web
+//            .ignoring()
+//            .antMatchers("/api/verifyIdToken");
+//    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors().and()
+                .csrf().disable()
+                .addFilterBefore(new FireBaseTokenFilter(), UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement().sessionCreationPolicy(STATELESS).and()
+                .authorizeRequests()
+                .antMatchers(AUTH_WHITELIST).permitAll()
+                .anyRequest().authenticated();
+
+        http.headers().frameOptions().disable();
+    }
+}
