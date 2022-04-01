91e9d27154498bbec0aeba15a9371bd302f015dc
gabouryn <ngaboury@gmail.com>
2022-04-01 11:20:11 -0700

    fix: improve message

--- src/main/java/com/zoomers/GameSetMatch/services/MatchService.java
+++ src/main/java/com/zoomers/GameSetMatch/services/MatchService.java
@@ -6,6 +6,7 @@ import com.zoomers.GameSetMatch.services.Errors.ProposedMatchChangeConflictExcep
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -16,15 +17,19 @@ public class MatchService {
 
     public void checkNewMatchTime(int tournamentID, int matchID, String newMatchAsAvailabilityString, int dayOfWeek) throws ProposedMatchChangeConflictException {
         List<ParticipantAvailabilityForADayInfo> participants = availabilityRepository.getParticipantsAvailabilityForADay(tournamentID, matchID, dayOfWeek);
-
+        List<String> conflictsWith = new ArrayList<>();
         int newMatchSlot = Integer.parseInt(newMatchAsAvailabilityString, 2);
 
         for (ParticipantAvailabilityForADayInfo participant : participants) {
             int participantsAvail = Integer.parseInt(participant.getAvailabilityString(), 2);
 
             if ((participantsAvail & newMatchSlot) == 0) {
-                throw new ProposedMatchChangeConflictException(String.format("Conflicts with %s's availability", participant.getName()));
+                conflictsWith.add(participant.getName());
             }
+        }
+
+        if (conflictsWith.size() > 0) {
+            throw new ProposedMatchChangeConflictException(String.format("Conflicts with the availability for: %s", String.join(", ", conflictsWith)));
         }
     }
 
