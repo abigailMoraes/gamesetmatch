17a1baa2ae46bdc8ed947b07bba330afa11940ed
abigailMoraes <98375256+abigailMoraes@users.noreply.github.com>
2022-04-01 23:04:02 -0700

    Update UserMatchTournamentRepository.java

--- src/main/java/com/zoomers/GameSetMatch/repository/UserMatchTournamentRepository.java
+++ src/main/java/com/zoomers/GameSetMatch/repository/UserMatchTournamentRepository.java
@@ -11,28 +11,30 @@ import java.util.List;
 public interface UserMatchTournamentRepository extends JpaRepository<UserMatchTournamentInfo, Long> {
 
 
-    @Query(value = "SELECT * FROM Tournament WHERE status = 5 AND + \n"
+    @Query(value = "SELECT * FROM Tournament WHERE status = :status AND + \n"
             +"EXISTS (SELECT * FROM user_registers_tournament WHERE user_registers_tournament.userID = :userID \n" +
             " AND Tournament.tournamentID = user_registers_tournament.tournamentID)",
             nativeQuery = true)
-    List<UserMatchTournamentInfo> getCompletedTournamentsForUser(int userID);
+    List<UserMatchTournamentInfo> getCompletedTournamentsForUser(int userID, int status);
 
     @Query(
            value ="SELECT u.results, u.attendance, Match_Has.matchID, Match_Has.start_time, Match_Has.end_time,\n" +
-                   "Round_Has.roundNumber,Tournament.name,Tournament.location,Tournament.description \n" +
+                   "Round_Has.roundNumber,t.name,t.location,t.description \n" +
                    "FROM (SELECT * FROM User_involves_match WHERE userID = :id) \n " +
                    "u LEFT JOIN Match_Has ON Match_Has.matchID = u.matchID LEFT JOIN \n" +
-                   " Round_Has ON Match_Has.roundID = Round_Has.roundID LEFT JOIN Tournament \n" +
-                   " ON Round_Has.tournamentID = Tournament.tournamentID;",
+                   " Round_Has ON Match_Has.roundID = Round_Has.roundID LEFT JOIN (SELECT * FROM Tournament WHERE \n" +
+            "status >= :status) t\n" +
+            " ON Round_Has.tournamentID = t.tournamentID;",
             nativeQuery = true)
     List<UserMatchTournamentInfo> findMatchesByUserID(int id);
 
     @Query(  value ="SELECT u.results, u.attendance, m.matchID, m.start_time, m.end_time,\n" +
-            "Tournament.name, Tournament.location, Tournament.description \n" +
+            "t.name, t.location, t.description \n" +
             "FROM (SELECT * FROM User_involves_match WHERE userID = :id) u \n" +
             "JOIN (SELECT * FROM Match_Has WHERE end_time <= NOW()) m ON m.matchID = u.matchID LEFT JOIN \n" +
-            " Round_Has ON m.roundID = Round_Has.roundID LEFT JOIN Tournament \n" +
-            " ON Round_Has.tournamentID = Tournament.tournamentID;",
+            " Round_Has ON m.roundID = Round_Has.roundID LEFT JOIN (SELECT * FROM Tournament WHERE \n" +
+            "status >= :status) t\n" +
+            " ON Round_Has.tournamentID = t.tournamentID;",
             nativeQuery = true)
     List<UserMatchTournamentInfo> findPastMatchesByUserID(int id);
 
