54c53281f5428a434f674c58966d144acc262f03
l8k1b <abigail.theresa.moraes@gmail.com>
2023-09-10 15:19:46 -0700

    Added code to read firebase key from environment variable

--- src/main/java/com/zoomers/GameSetMatch/GameSetMatchApplication.java
+++ src/main/java/com/zoomers/GameSetMatch/GameSetMatchApplication.java
@@ -1,34 +1,43 @@
 package com.zoomers.GameSetMatch;
-
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
-
+import java.nio.charset.StandardCharsets;
 
 import java.io.*;
 
 @SpringBootApplication
 @ComponentScan(basePackages= { "com.zoomers" })
 public class GameSetMatchApplication {
+	@Value("${FIREBASE_KEY}")
+    private String serviceAccountKeyFile;
+
 	public static void main(String[] args) throws IOException {
-        Resource resource = new ClassPathResource("Firebase_ServiceAccountKey.json");
+        //Resource resource = new ClassPathResource("Firebase_ServiceAccountKey.json");
+		String firebaseKey = System.getenv("FIREBASE_KEY");
+    	System.out.println("FIREBASE_KEY: " + firebaseKey);
+		GameSetMatchApplication application = new GameSetMatchApplication();
+        application.initializeFirebaseAndRun(firebaseKey, args);
 
-		InputStream serviceAccount = resource.getInputStream();
+		
+	}
+    public void initializeFirebaseAndRun(String serviceAccountKeyFile, String[] args) throws IOException {
+		InputStream serviceAccount = new ByteArrayInputStream(serviceAccountKeyFile.getBytes());
+			System.out.println("FIREBASE_KEY: " + serviceAccount);
+			FirebaseOptions options = FirebaseOptions.builder()
+					.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+					.build();
 
-		FirebaseOptions options = FirebaseOptions.builder()
-				.setCredentials(GoogleCredentials.fromStream(serviceAccount))
-				.build();
+			FirebaseApp.initializeApp(options);
 
-		FirebaseApp.initializeApp(options);
-
-
-		SpringApplication.run(GameSetMatchApplication.class, args);
+			SpringApplication.run(GameSetMatchApplication.class, args);
 	}
 
 }
